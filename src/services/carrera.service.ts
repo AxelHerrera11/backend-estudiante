8f6c2d6e39c4af6974e91beaec8b44d623aa965e
AxelHerrera11 <aherreras@miumg.edu.gt>
2025-03-22 08:47:56 -0600

    Subida Segunda Clase

---
+++ src/services/carrera.service.ts
@@ -0,0 +1,64 @@
+import { AppDataSourse } from "../config/db";
+import { Carrera } from "../entities/Carrera.entity";
+
+// Crear el repositorio
+const carreraRepository = AppDataSourse.getRepository(Carrera);
+
+// C = Create, R = Read, = U = Update, D = Delete
+
+// Leer todas las carreras
+export const srvGetCarreras = async () => {
+    // select * from carreras order by id_carrera desc;
+    const carreras = await carreraRepository.find({
+        order: {nombreCarrera: 'ASC'}
+    });
+
+    return carreras;
+}
+
+// Obtener una carrera
+export const srvGetCarreraByID = async (pIdCCarrera: number) => {
+    const carrera = await carreraRepository.findOne({
+        where: { idCarrera: pIdCCarrera}
+    });
+
+    return carrera;
+}
+
+// Crear una nueva carrera
+export const srvCreateCarrera = async (pNombreCarrera: string) => {
+    const nuevaCarrera = new Carrera();
+
+    nuevaCarrera.nombreCarrera = pNombreCarrera;
+
+    return await carreraRepository.save(nuevaCarrera);
+
+}
+
+// Actualizar carrera
+export const srvUpdateCarrera = async (pIdCarrera: number, pNombreCarrera: string) => {
+    // Buscar carrera
+    const carrera = await carreraRepository.findOne({
+        where: {idCarrera: pIdCarrera}
+    });
+
+    //validacion
+    if(!carrera) return null;
+
+    carrera.nombreCarrera = pNombreCarrera;
+
+    return await carreraRepository.save(carrera);
+}
+
+// Eliminar Carrera
+export const srcDeleteCarrera = async (pIdCarrera: number) => {
+    // Buscar carrera
+    const carrera = await carreraRepository.findOne({
+        where: {idCarrera: pIdCarrera}
+    });
+
+    //validacion
+    if(!carrera) return null;
+
+    return await carreraRepository.remove(carrera);
+}
